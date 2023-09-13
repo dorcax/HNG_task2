a71230a85ba591df4177373ea45d017542def9fe
dorcax <dorcasibrahim667@gmail.com>
2023-09-13 23:48:20 +0100

    corrected the mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const app = express()
-// const connectDb = require("./connectDb/connectDB")
-const connectDB =require("./connectDb/connectDB")
-require("dotenv").config({path:__dirname+'/.env'})
+require("dotenv").config()
 const port = process.env.PORT || 3000
 const errorHandler =require("./error/errorHandlers")
 const router =require("./Router/router")
@@ -12,14 +10,19 @@ app.use(express.json())
 
 app.use("/api", router)
 
+
+
+
+ mongoose.connect(process.env.MONGO_URL, {  useNewUrlParser: true, useUnifiedTopology: true, })
+    .then(() => {
+      console.log("connected to the DB");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
 app.use(errorHandler)
 
-
-const start = async () => {
-    await connectDB(process.env.MONGO_URL);
-    console.log(process.env.MONGO_URL)
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
-  });
-};
-start();
+  })
