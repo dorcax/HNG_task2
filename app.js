612c5d6cb4517894661dc7c3765492efd28adc73
dorcax <dorcasibrahim667@gmail.com>
2023-09-14 00:17:08 +0100

    database

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require("express")
 const app = express()
 const path =require("path")
-require("dotenv").config({path:path.join(__dirname,".env")})
+
+const dotenv = require("dotenv")
+dotenv.config({ path: path.join(__dirname,".env") })
 const mongoose =require("mongoose")
 const port = process.env.PORT || 3000
 const errorHandler =require("./error/errorHandlers")
-const router =require("./Router/router")
+const router = require("./Router/router")
+const source =process.env.MONGO_URL
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-
-
 app.use("/api", router)
 
 
 
 
- mongoose.connect(process.env.MONGO_URL, {  useNewUrlParser: true, useUnifiedTopology: true, })
+ mongoose.connect(source, {  useNewUrlParser: true, useUnifiedTopology: true, })
     .then(() => {
       console.log("connected to the DB");
     })
