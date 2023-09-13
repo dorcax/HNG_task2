49c63ff10aafec8b923635d8831d7e72a6da1ba1
dorcax <dorcasibrahim667@gmail.com>
2023-09-13 23:51:22 +0100

    did some correction

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
+const mongoose =require("mongoose")
 const port = process.env.PORT || 3000
 const errorHandler =require("./error/errorHandlers")
 const router =require("./Router/router")
